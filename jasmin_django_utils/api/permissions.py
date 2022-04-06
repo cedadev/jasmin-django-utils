ee5839623178f922effcd261f655c5d7f42ccef1
Alex Manning <alexander.manning@stfc.ac.uk>
2022-04-06 12:28:43 +0100

    Tweak permission class ancestor.

--- jasmin_django_utils/api/permissions.py
+++ jasmin_django_utils/api/permissions.py
@@ -3,10 +3,8 @@ import oauth2_provider.contrib.rest_framework as oauth2_rf
 import rest_framework.permissions as rf_perms
 
 
-class IsAdminUserOrTokenHasResourceScope(rf_perms.OperandHolder):
+class IsAdminUserOrTokenHasResourceScope(rf_perms.OR):
     """Permission class to allow admin user access OR correctly scoped OAuth2 tokens."""
 
     def __init__(self):
-        super().__init__(
-            rf_perms.OR, oauth2_rf.TokenHasResourceScope, rf_perms.IsAdminUser
-        )
+        super().__init__(oauth2_rf.TokenHasResourceScope, rf_perms.IsAdminUser)
