896a070582a5d090274660287d245d1980b2041b
Alex Manning <alexander.manning@stfc.ac.uk>
2024-04-04 09:56:18 +0100

    Convert packaging to poetry.

--- setup.py
+++ setup.py
@@ -1,36 +1,3 @@
-#!/usr/bin/env python3
+import setuptools
 
-import os
-
-from setuptools import find_packages, setup
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(here, "README.md")) as f:
-    README = f.read()
-
-if __name__ == "__main__":
-    setup(
-        name="jasmin-django-utils",
-        version="1.0.6",
-        description="Django utilities used by JASMIN apps.",
-        long_description=README,
-        classifiers=[
-            "Programming Language :: Python",
-            "Framework :: Django",
-        ],
-        author="Matt Pryor",
-        author_email="matt.pryor@stfc.ac.uk",
-        url="https://github.com/cedadev/jasmin-django-utils",
-        keywords="web django jasmin utilities utils enumfield crossdb settings appsettings",
-        packages=find_packages(),
-        include_package_data=True,
-        zip_safe=False,
-        install_requires=["django"],
-        extras_require={
-            "api": [
-                "djangorestframework",
-                "django-oauth-toolkit",
-            ],
-        },
-    )
+setuptools.setup()
